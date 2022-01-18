d30de0eee4280e7c51807e6739f8fb1fc2c52955
Gerald Annan <annang2@stanbic.com.gh>
2022-01-18 19:10:05 +0000

    Implements the `Newsletter.log_sent_email/1` function

--- lib/newsletter.ex
+++ lib/newsletter.ex
@@ -10,11 +10,10 @@ defmodule Newsletter do
   end
 
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    IO.puts(pid, email)
   end
 
   def close_log(pid) do
-    # Please implement the close_log/1 function
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
